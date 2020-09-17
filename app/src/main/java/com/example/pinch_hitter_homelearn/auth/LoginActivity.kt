77ee33071d52d69a50373ca3d46eb84aa0308fa5
WooSangGyu <wsg0701@naver.com>
2020-09-17 22:23:22 +0900

    Change Login Activity and Making Sign up Activity

--- app/src/main/java/com/example/pinch_hitter_homelearn/auth/LoginActivity.kt
+++ app/src/main/java/com/example/pinch_hitter_homelearn/auth/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.example.pinch_hitter_homelearn.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pinch_hitter_homelearn.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        signup_btn.setOnClickListener {
+            val signUpIntent = Intent(this@LoginActivity, SignUpActivity::class.java)
+            startActivity(signUpIntent)
+        }
     }
 }
--- app/src/main/java/com/example/pinch_hitter_homelearn/auth/SignUpActivity.kt
+++ app/src/main/java/com/example/pinch_hitter_homelearn/auth/SignUpActivity.kt
@@ -1,12 +1,15 @@
 package com.example.pinch_hitter_homelearn.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pinch_hitter_homelearn.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
     }
 }
