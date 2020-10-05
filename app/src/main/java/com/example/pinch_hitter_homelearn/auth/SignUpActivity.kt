675c6602a78fd02e863226c4ad47c290e6fc9ba9
WooSangGyu <wsg0701@naver.com>
2020-10-05 23:35:22 +0900

    SignUpActivity1 complete, So Next Page Making

--- app/src/main/java/com/example/pinch_hitter_homelearn/auth/SignUpActivity.kt
+++ app/src/main/java/com/example/pinch_hitter_homelearn/auth/SignUpActivity.kt
@@ -3,6 +3,8 @@ package com.example.pinch_hitter_homelearn.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
@@ -12,7 +14,13 @@ import com.example.pinch_hitter_homelearn.R
 import com.example.pinch_hitter_homelearn.`interface`.ApiService
 import com.example.pinch_hitter_homelearn.`interface`.SignUpClass
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up2.*
 import kotlinx.android.synthetic.main.activity_signup.*
+import kotlinx.android.synthetic.main.activity_signup.pw_find_answer
+import kotlinx.android.synthetic.main.activity_signup.pw_find_question_choice
+import kotlinx.android.synthetic.main.activity_signup.user_id_input
+import kotlinx.android.synthetic.main.activity_signup.user_pw_input
+import kotlinx.android.synthetic.main.activity_signup.view.*
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
@@ -20,11 +28,17 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
+import kotlin.collections.ArrayList
 
 class SignUpActivity : AppCompatActivity() {
 
     var id_check = true
     var hint_id = ""
+    var id_flag = false
+    var pw_flag = false
+    var pw_check_flag = false
+    var answer_flag = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,49 +51,116 @@ class SignUpActivity : AppCompatActivity() {
         pw_find_question_choice.adapter = spinnerAdapter
         hint_id = pw_find_question_choice.selectedItemPosition.toString()
         hint_id = hint_id +1
-        signUpBtn.setOnClickListener {
 
-            var email = user_id_input.text.toString()
-            var pw = user_pw_input.text.toString()
-            var find_asnwer = pw_find_answer.text.toString()
-            var birth = birth_day.text.toString()
-            var name = user_name.text.toString()
-            var sex = Sex_Group.checkedRadioButtonId
-            var phoneNum = "01084802790"
-            var branch = user_sign_point.text.toString()
+        user_id_input.addTextChangedListener( object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
 
-            var role = JSONObject()
-            role.put("id", 1)
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
-            if(user_email != null && id_check == true && user_password != null && pw_find_answer != null && user_name != null && birth_day != null && sex != null) {
-                var gender = resources.getResourceEntryName(sex)
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, count: Int) {
+                if (count == 0) {
+                    id_flag = false
+                } else if (count > 0) {
+                    id_flag = true
+                }
 
-                val memberObject = JSONObject()
-                memberObject.put("loginId", email)
-                memberObject.put("passWord", pw)
-                memberObject.put("sns", "None")
-                memberObject.put("name", name)
-                memberObject.put("birthDay", birth)
-                memberObject.put("sex", gender)
-                memberObject.put("phone", phoneNum)
-                memberObject.put("branchId", branch)
-                memberObject.put("role", role)
+                if(id_flag && pw_flag && pw_check_flag && answer_flag) {
+                    next_btn.setBackgroundResource(R.drawable.sign_up_button2)
+                } else {
+                    next_btn.setBackgroundResource(R.drawable.sign_up_button)
+                }
+            }
 
-                println(memberObject)
+        })
 
-                apiconnect.signUpApi(memberObject, 1, pw_find_answer.text.toString()).enqueue(object : Callback<SignUpClass> {
-                    override fun onFailure(call: Call<SignUpClass>, t: Throwable) {
-                        t.message?.let { it1 -> Log.d("DEBUG", it1)}
-                        println("여기로 타는중")
-                        Toast.makeText(applicationContext, "서버 통신 오류", Toast.LENGTH_SHORT).show()
+        user_pw_input.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, count: Int) {
+                if (count == 0) {
+                    pw_flag = false
+                } else if (count > 0) {
+                    pw_flag = true
+                }
+
+                if(id_flag && pw_flag && pw_check_flag && answer_flag) {
+                    next_btn.setBackgroundResource(R.drawable.sign_up_button2)
+                } else {
+                    next_btn.setBackgroundResource(R.drawable.sign_up_button)
+                }
+            }
+
+        })
+
+        user_pw_input_check.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, count: Int) {
+                if (count == 0) {
+                    pw_check_flag = false
+                } else if (count > 0) {
+                    pw_check_flag = true
+                }
+
+                if(id_flag && pw_flag && pw_check_flag && answer_flag) {
+                    next_btn.setBackgroundResource(R.drawable.sign_up_button2)
+                } else {
+                    next_btn.setBackgroundResource(R.drawable.sign_up_button)
+                }
+            }
+
+        })
+
+        pw_find_answer.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, count: Int) {
+                if (count == 0) {
+                    answer_flag = false
+                } else if (count > 0) {
+                    answer_flag = true
+                }
+
+                if(id_flag && pw_flag && pw_check_flag && answer_flag) {
+                    next_btn.setBackgroundResource(R.drawable.sign_up_button2)
+                } else {
+                    next_btn.setBackgroundResource(R.drawable.sign_up_button)
+                }
+            }
+        })
+
+        next_btn.setOnClickListener {
+            if(id_flag && pw_flag && pw_check_flag && answer_flag) {
+                val email = user_id_input.text.toString()
+                val pw = user_pw_input.text.toString()
+                val pw_check = user_pw_input_check.text.toString()
+                val find_answer = pw_find_answer.text.toString()
+
+                if( pw == pw_check) {
+                    if( email != null && pw != null && pw_check != null && find_answer != null && id_check == true) {
+                        val intent = Intent(this@SignUpActivity, SignUpActivity2::class.java)
+                        intent.putExtra("email", email)
+                        intent.putExtra("pw", pw)
+                        intent.putExtra("findAnswer", find_answer)
+                        startActivity(intent)
                     }
-
-                    override fun onResponse(call: Call<SignUpClass>, response: Response<SignUpClass>) {
-                        val result = response.body();
-                        println("여기로 탐" + result)
-                    }
-
-                })
+                } else {
+                    Toast.makeText(this, "비밀번호가 다릅니다.",Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
--- app/src/main/java/com/example/pinch_hitter_homelearn/auth/SignUpActivity2.kt
+++ app/src/main/java/com/example/pinch_hitter_homelearn/auth/SignUpActivity2.kt
@@ -2,11 +2,54 @@ package com.example.pinch_hitter_homelearn.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.pinch_hitter_homelearn.R
+import com.example.pinch_hitter_homelearn.`interface`.SignUpClass
+import kotlinx.android.synthetic.main.activity_signup.*
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up2)
+
+
+//        var role = JSONObject()
+//        role.put("id", 1)
+//
+//        if(user_email != null && id_check == true && user_password != null && pw_find_answer != null && user_name != null && birth_day != null && sex != null) {
+//            var gender = resources.getResourceEntryName(sex)
+//
+//            val memberObject = JSONObject()
+//            memberObject.put("loginId", email)
+//            memberObject.put("passWord", pw)
+//            memberObject.put("sns", "None")
+//            memberObject.put("name", name)
+//            memberObject.put("birthDay", birth)
+//            memberObject.put("sex", gender)
+//            memberObject.put("phone", phoneNum)
+//            memberObject.put("branchId", branch)
+//            memberObject.put("role", role)
+//
+//            println(memberObject)
+//
+//            apiconnect.signUpApi(memberObject, 1, pw_find_answer.text.toString()).enqueue(object : Callback<SignUpClass> {
+//                override fun onFailure(call: Call<SignUpClass>, t: Throwable) {
+//                    t.message?.let { it1 -> Log.d("DEBUG", it1)}
+//                    println("여기로 타는중")
+//                    Toast.makeText(applicationContext, "서버 통신 오류", Toast.LENGTH_SHORT).show()
+//                }
+//
+//                override fun onResponse(call: Call<SignUpClass>, response: Response<SignUpClass>) {
+//                    val result = response.body();
+//                    println("여기로 탐" + result)
+//                }
+//
+//            })
+//        }
     }
 }
